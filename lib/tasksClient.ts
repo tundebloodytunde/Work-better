18379b8253567d8b75260de4b515a339b0d28108
tundebloodytunde <6qvsyrsb9f@privaterelay.appleid.com>
2026-06-19 16:26:26 -0400

    Add headers to updateTaskStatus fetch request

--- lib/tasksClient.ts
+++ lib/tasksClient.ts
@@ -1,6 +1,9 @@
 export async function updateTaskStatus(id: number, status: string) {
   await fetch("/api/tasks", {
     method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify({ id, status }),
   });
 }
